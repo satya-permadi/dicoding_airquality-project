0ca1a351fb2a8e803d55fc5b67a078fe9ea07b40
Satya Permadi <satyaintershuty@outlook.com>
2025-01-31 14:16:22 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 def load_data():
     # Memuat dataset Aotizhongxin, Changping, Dingling
     df_all = pd.read_csv("https://raw.githubusercontent.com/satya-permadi/dicoding_airquality-project/refs/heads/main/AirQuality_Dataset/PRSA_Data_Nongzhanguan_20130301-20170228.csv")
+    # Tambahkan kolom 'date_time'
+    df_all['date_time'] = pd.to_datetime(df_all[['year', 'month', 'day', 'hour']])
     return df_all
 
 # Panggil fungsi untuk memuat data
@@ -15,7 +17,7 @@ df_all = load_data()
 
 # Sidebar untuk navigasi
 st.sidebar.title("Menu Navigasi")
-menu = st.sidebar.selectbox("Pilih Analisis:", ["Home", "Pertanyaan Satu", "Pertanyaan Dua"])
+menu = st.sidebar.selectbox("Pilih Analisis:", ["Home", "Pertanyaan 1", "Pertanyaan 2"])
 
 # Home Section
 if menu == "Home":
@@ -48,25 +50,24 @@ elif menu == "Pertanyaan 1":
 
         monthly_avg['date_time_month'] = monthly_avg['date_time_month'].dt.to_timestamp()
 
-        plt.figure(figsize=(14, 8))
+        fig, ax = plt.subplots(figsize=(14, 8))
 
-        sns.lineplot(x='date_time_month', y='CO', data=monthly_avg, label='CO', color='blue')
-        sns.lineplot(x='date_time_month', y='NO2', data=monthly_avg, label='NO2', color='red')
-        sns.lineplot(x='date_time_month', y='PM2.5', data=monthly_avg, label='PM2.5', color='green')
-        sns.lineplot(x='date_time_month', y='PM10', data=monthly_avg, label='PM10', color='purple')
-        sns.lineplot(x='date_time_month', y='SO2', data=monthly_avg, label='SO2', color='orange')
-        sns.lineplot(x='date_time_month', y='O3', data=monthly_avg, label='O3', color='cyan')
+        sns.lineplot(x='date_time_month', y='CO', data=monthly_avg, label='CO', color='blue', ax=ax)
+        sns.lineplot(x='date_time_month', y='NO2', data=monthly_avg, label='NO2', color='red', ax=ax)
+        sns.lineplot(x='date_time_month', y='PM2.5', data=monthly_avg, label='PM2.5', color='green', ax=ax)
+        sns.lineplot(x='date_time_month', y='PM10', data=monthly_avg, label='PM10', color='purple', ax=ax)
+        sns.lineplot(x='date_time_month', y='SO2', data=monthly_avg, label='SO2', color='orange', ax=ax)
+        sns.lineplot(x='date_time_month', y='O3', data=monthly_avg, label='O3', color='cyan', ax=ax)
 
-        plt.title('Konsentrasi CO dan NO2 Selama Beberapa Tahun (Rata-rata Bulanan)')
-        plt.xlabel('Bulan')
-        plt.ylabel('Konsentrasi Polutan')
+        ax.set_title('Konsentrasi CO dan NO2 Selama Beberapa Tahun (Rata-rata Bulanan)')
+        ax.set_xlabel('Bulan')
+        ax.set_ylabel('Konsentrasi Polutan')
 
-        plt.xticks(ticks=monthly_avg['date_time_month'], labels=monthly_avg['date_time_month'].dt.strftime('%Y-%m'), rotation=45)
-
+        plt.xticks(rotation=45)
         plt.legend()
         plt.grid(True)
         plt.tight_layout()
-        st.pyplot(plt)
+        st.pyplot(fig)
  
 elif menu == "Pertanyaan 2":
     st.title("Bagaimana pengaruh hujan terhadap polutan penyebab polusi udara di nongzhanguan?")
@@ -79,26 +80,23 @@ elif menu == "Pertanyaan 2":
 
         df_all['RAIN_GROUP'] = pd.cut(df_all['RAIN'], bins=[0, 1, 4, 8, 10], labels=['Tidak Ada Hujan', 'Hujan Ringan', 'Hujan Sedang', 'Hujan Lebat'])
 
-        plt.figure(figsize=(12, 8))
-        sns.boxplot(x='RAIN_GROUP', y='PM2.5', data=df_all)
-        plt.title('Visualisasi Data dengan Box Plot antara PM2.5 dengan Intensitas Hujan di Nongzhanguan')
+        fig, ax = plt.subplots(figsize=(12, 8))
+        sns.boxplot(x='RAIN_GROUP', y='PM2.5', data=df_all, ax=ax)
+        ax.set_title('Visualisasi Data dengan Box Plot antara PM2.5 dengan Intensitas Hujan di Nongzhanguan')
         plt.xticks(rotation=45)
-        st.pyplot(plt)
-
-        df_all['RAIN_GROUP'] = pd.cut(df_all['RAIN'], bins=[0, 1, 4, 8, 10], labels=['Tidak Ada Hujan', 'Hujan Ringan', 'Hujan Sedang', 'Hujan Lebatn'])
+        st.pyplot(fig)
 
         rain_group_avg = df_all.groupby('RAIN_GROUP')['PM2.5'].mean().reset_index()
 
-        plt.figure(figsize=(12, 6))
-        sns.lineplot(x='RAIN_GROUP', y='PM2.5', data=rain_group_avg, marker='o', color='blue')
+        fig, ax = plt.subplots(figsize=(12, 6))
+        sns.lineplot(x='RAIN_GROUP', y='PM2.5', data=rain_group_avg, marker='o', color='blue', ax=ax)
 
-        plt.title('Rata-rata Konsentrasi PM2.5 Berdasarkan Intensitas Hujan')
-        plt.xlabel('Kelompok Intensitas Hujan')
-        plt.ylabel('Rata-rata Konsentrasi PM2.5')
+        ax.set_title('Rata-rata Konsentrasi PM2.5 Berdasarkan Intensitas Hujan')
+        ax.set_xlabel('Kelompok Intensitas Hujan')
+        ax.set_ylabel('Rata-rata Konsentrasi PM2.5')
 
-        st.pyplot(plt)
+        st.pyplot(fig)
 
- 
         st.subheader("Kesimpulan")
         st.markdown("""
         Dari analisis di atas, dapat disimpulkan bahwa:
@@ -106,6 +104,4 @@ elif menu == "Pertanyaan 2":
         1. **Peningkatan konsentrasi CO dan NO₂** Tingkat karbon monoksida (CO) dan nitrogen dioksida (NO₂) yang meningkat di Nongzhanguan mengindikasikan adanya aktivitas kendaraan bermotor atau pembakaran bahan bakar fosil yang intensif. Hal ini menegaskan bahwa kedua sumber tersebut merupakan kontributor utama polutan udara di wilayah tersebut.
 
         2. **Hujan dengan Peningkatan PM2.5** data penelitian di Nongzhanguan membuktikan bahwa hujan berperan menurunkan kadar polutan, salah satunya PM2.5. Fenomena ini terjadi karena air hujan membawa serta polutan, sehingga distribusi PM2.5 mengalami penurunan drastis. Hujan berpotensi membersihkan partikel polutan dari udara (wet deposition). Intensitas hujan yang lebih tinggi berkontribusi pada penurunan PM2.5 ke level terendah. Sebaliknya, intensitas hujan rendah dapat meningkatkan intensitas PM2.5 di Nongzhanguan.
-
-        
         """)
